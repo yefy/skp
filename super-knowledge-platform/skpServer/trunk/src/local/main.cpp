35def9f1f1bf220b756f4b21d6738b6d17bd2f98
yefy <yefy@localhost.localdomain>
2016-03-22 10:33:35 +0800

    commit test

--- super-knowledge-platform/skpServer/trunk/src/local/main.cpp
+++ super-knowledge-platform/skpServer/trunk/src/local/main.cpp
@@ -1,14 +1,14 @@
 #include "skp_local_application.h"
 #include "skpLog.h"
 #include "gmock/gmock.h"
-//#include <gperftools/profiler.h>
+//---#include <gperftools/profiler.h>
 //#include <gperftools/heap-profiler.h>
 
 SkpLog *g_log = skp_null;
 
 int main(int argc, char *argv[])
 {
-    //ProfilerStart(NULL);
+    //---ProfilerStart(NULL);
     //HeapProfilerStart(NULL);
 
     g_log = new SkpLog("", "main_server_", Skp::LOG_ERROR, /*Skp::LOG_TO_FILE | */Skp::LOG_TO_STDERR,skp_true,0,1024 * 1024 *100, skp_true);
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     SkpLocalApplication app(argc, argv);
     int res = app.exec();
 
-    //ProfilerStop();
+    //---ProfilerStop();
     //HeapProfilerStop();
     return res;
 }
